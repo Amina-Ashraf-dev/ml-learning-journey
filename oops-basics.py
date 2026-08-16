e21fb9befdca66b609823b7f7d5373850e936fdb
Amina Ashraf <aminakhanum44@gmail.com>
2026-08-17 01:03:46 +0500

    oops inheritance and polymorphism

--- oops-basics.py
+++ oops-basics.py
@@ -5,10 +5,16 @@ class Patient:
         self.diagnose = diagnose
     def display(self):
         print(f"{self.name} is {self.age} years old and has been diagnosed with {self.diagnose}")
+class CancerPatient(Patient):
+    def __init__(self, name, age, diagnose, stage):
+        super().__init__(name, age, diagnose)
+        self.stage = stage
+    def display(self):
+        print(f"{self.name}is{self.age}years old has been diagnosed with{self.diagnose} and stage is {self.stage}")   
 Patient1=Patient("Ali", 45, "Flu")  
-Patient2=Patient("Sara", 30, "Diabetes")
-Patient3=Patient("John", 60, "Hypertension")  
+Patient2=CancerPatient("Sara", 30, "Diabetes", "stage2")
+#Patient3=Patient("John", 60, "Hypertension")  
 Patient1.display()
 Patient2.display()
-Patient3.display()
+#Patient3.display()
 
