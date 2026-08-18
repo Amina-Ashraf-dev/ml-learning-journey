5ab0e9bf743974cb105dcc1520072e8cad33334b
Amina Ashraf <aminakhanum44@gmail.com>
2026-08-18 12:05:40 +0500

    oop completed - encapsulation and abstraction

--- oops-basics.py
+++ oops-basics.py
@@ -1,4 +1,4 @@
-class Patient:
+"""class Patient:
     def __init__(self, name, age, diagnose):
         self.name = name
         self.age = age
@@ -10,11 +10,41 @@ class CancerPatient(Patient):
         super().__init__(name, age, diagnose)
         self.stage = stage
     def display(self):
-        print(f"{self.name}is{self.age}years old has been diagnosed with{self.diagnose} and stage is {self.stage}")   
-Patient1=Patient("Ali", 45, "Flu")  
-Patient2=CancerPatient("Sara", 30, "Diabetes", "stage2")
+        print(f"{self.name} is {self.age} years old and has been diagnosed with {self.diagnose} and stage is {self.stage}")
+#Patient1=Patient("Ali", 45, "Flu")  
+#Patient2=CancerPatient("Sara", 30, "Diabetes", "stage2")
 #Patient3=Patient("John", 60, "Hypertension")  
-Patient1.display()
-Patient2.display()
+#Patient1.display()
+#Patient2.display()p
 #Patient3.display()
-
+Patients=[Patient("Ali",45,"Flu"),CancerPatient("Sara",30, "Lymphoma","Stage 2")]
+for Patient in Patients:
+    Patient.display()"""
+"""class Patient:
+    def __init__(self, name, age, diagnose):
+        self.__name=name
+        self.__age=age
+        self.__diagnose=diagnose
+    def get_name(self):
+        return self.__name
+    def get_age(self):
+        return self.__age
+    def get_diagnose(self):
+        return self.__diagnose
+Patient1=Patient("Ali", 45, "Flu")
+"""
+"""print(Patient1.get_name())
+print(Patient1.get_age())  
+print (Patient1.get_diagnose())"""
+#print(Patient1.__name)
+from abc import ABC, abstractmethod
+class Hospital(ABC):
+    @abstractmethod
+    def treat_patient(self):
+        pass
+class LymphomaCenter(Hospital):
+    def treat_patient(self):
+        print("Administering lymphoma center treatment")
+center=LymphomaCenter() 
+center.treat_patient()       
+h=Hospital()
